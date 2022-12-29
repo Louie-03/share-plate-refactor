e1825816143110dbf1721edd557dac2db47b9b30
Louie-03 <dhdustnr0134@naver.com>
2022-12-29 15:13:28 +0900

    feat : 문자열 관련 유틸 클래스 구현
    문자열이 null 또는 빈 문자열인지 확인하는 isBlank 메서드 구현

---
+++ src/main/java/louie/hanse/shareplate/common/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package louie.hanse.shareplate.common.utils;
+
+public class StringUtils {
+
+    public static boolean isBlank(String string) {
+        return string == null || string.isBlank();
+    }
+}
