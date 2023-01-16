7fcad8e2c75a1b1558b943406e2c501fd06c04a8
Louie-03 <dhdustnr0134@naver.com>
2023-01-16 11:44:19 +0900

    chore : 유효하지 않은 위도, 경도에 대한 예외 메시지 수정

--- src/main/java/louie/hanse/shareplate/common/exception/invalid/InvalidLongitudeException.java
+++ src/main/java/louie/hanse/shareplate/common/exception/invalid/InvalidLongitudeException.java
@@ -2,7 +2,7 @@ package louie.hanse.shareplate.common.exception.invalid;
 
 public class InvalidLongitudeException extends InvalidException {
 
-    private static final String message = "유효하지 않은 경도입니다.";
+    private static final String message = "유효하지 않은 경도값입니다.";
 
     public InvalidLongitudeException() {
         super(message);
