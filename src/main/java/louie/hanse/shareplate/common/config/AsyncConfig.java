b0624a10b0e0589b9e2ad2b18169baa1dbe30918
Louie-03 <dhdustnr0134@naver.com>
2023-02-23 09:09:52 +0900

    refactor : 비동기 관련 설정 별도의 클래스로 분리, 비동기 처리를 담당하는 ThreadPoolTaskExecutor 빈 등록

---
+++ src/main/java/louie/hanse/shareplate/common/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package louie.hanse.shareplate.common.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return threadPoolTaskExecutor();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("async-thread-");
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
+}
