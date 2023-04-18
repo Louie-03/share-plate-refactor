9bf0c505afaf2111d4f19bada414798f118ec3de
Louie-03 <dhdustnr0134@naver.com>
2023-04-18 18:58:28 +0900

    refactor : 설정 관련 어노테이션의 위치 변경

--- src/main/java/louie/hanse/shareplate/SharePlateApplication.java
+++ src/main/java/louie/hanse/shareplate/SharePlateApplication.java
@@ -2,12 +2,8 @@ package louie.hanse.shareplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@ConfigurationPropertiesScan
 public class SharePlateApplication {
 
 	public static void main(String[] args) {
